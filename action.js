81469f051ac33d8b94a7015b6a9f9c1396923950
Abhay Kumar <actuallyabhay@gmail.com>
2024-08-03 16:51:48 +0530

    step-3 & 4 done

--- action.js
+++ action.js
@@ -1,4 +1,6 @@
-const choices = ["rock", "paper", "scissors"];
+const choices = ["rock", "paper", "scissor"];
+let humanScore = 0;
+let computerScore = 0;
 
 function getComputerChoice()
 {
@@ -6,4 +8,51 @@ function getComputerChoice()
     return choices[randIndex];
 }
 
-console.log(getComputerChoice());
+function getHumanChoice()
+{
+    let humanChoice = prompt("rock / paper / scissor? : ").toLowerCase();
+    for(;!choices.includes(humanChoice);)
+    {
+        humanChoice = prompt("Invalid input, rock / paper / scissor? : ").toLowerCase();
+    }
+    return humanChoice;
+}
+
+function playRound(humanChoice, computerChoice)
+{
+    const hc = humanChoice();
+    const cc = computerChoice();
+    
+    if(hc === cc)
+    {
+        console.log(`Draw! computer: ${cc} and your choice: ${hc}`);
+    }
+    else if((hc === choices[0] || hc === choices[2]) && (cc === "rock" || cc === "scissors"))
+    {
+        if (choices.indexOf(hc) > choices.indexOf(cc))
+        {
+            console.log(`You lose! ${cc} beats ${hc}.`);
+            computerScore += 1;
+        }else
+        {
+            console.log(`You win! ${hc} beats ${cc}`);
+            humanScore += 1;
+        }
+    } 
+    else
+    {
+        if (choices.indexOf(hc) < choices.indexOf(cc))
+        {
+            console.log(`You lose! ${cc} beats ${hc}.`);
+            computerScore += 1;
+        }else
+        {
+            console.log(`You win! ${hc} beats ${cc}`);
+            humanScore += 1;
+        }
+    }
+}
+
+// console.log(`Computer Choice: ${getComputerChoice()}`);
+// console.log(getHumanChoice());
+playRound(getHumanChoice, getComputerChoice);
