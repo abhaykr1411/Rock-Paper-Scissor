878d9a9bfc5843759be500a0c267bace7a9b65fa
Abhay <actuallyabhay@gmail.com>
2025-01-15 10:39:48 +0000

    completion

--- action.js
+++ action.js
@@ -1,6 +1,4 @@
 const choice = ["rock", "paper", "scissor"];
-let humanScore = 0,
-  computerScore = 0;
 
 function getComputerChoice() {
   let randomIndex = Math.floor(Math.random() * choice.length);
@@ -10,7 +8,7 @@ function getComputerChoice() {
 
 function getHumanChoice() {
   let humanChoice = prompt("Rock/paper/scissor: ").toLowerCase();
-  while (!(choice.includes(humanChoice))) {
+  while (!choice.includes(humanChoice)) {
     humanChoice = prompt(
       "Oops! wrong input (rock/paper/scissor): "
     ).toLowerCase();
@@ -20,34 +18,55 @@ function getHumanChoice() {
   return humanChoice;
 }
 
-function playRound(humanChoice, computerchoice)
-{
-    if(humanChoice === computerChoice)
-    {
-        console.log(`Draw b/w ${humanChoice} & ${computerChoice}`);
-    }else if((humanChoice == "scissor" && computerChoice == "rock") || (humanChoice == "rock" && computerChoice == "scissor"))
-    {
-        if(choice.indexOf(humanChoice) < choice.indexOf(computerChoice))
-        {
-            console.log(`you won ${humanChoice} beats ${computerChoice}`);
-        }
-        else {
-            console.log(`you lost ${computerChoice} beats ${humanChoice}`);
-        }
-    }
-    else
-    {
-        if(choice.indexOf(humanChoice) > choice.indexOf(computerChoice))
-        {
-            console.log(`you won ${humanChoice} beats ${computerChoice}`);
-        }
-        else
-        {
-            console.log(`you lost ${computerChoice} beats ${humanChoice}`);
-        }
-    }
-}
-let computerChoice = getComputerChoice();
-let humanChoice = getHumanChoice();
+function playGame() {
+  let humanScore = 0,
+    computerScore = 0;
 
-playRound(humanChoice, computerChoice);
+  function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+      console.log(`Draw b/w ${humanChoice} & ${computerChoice}`);
+    } else if (
+      (humanChoice == "scissor" && computerChoice == "rock") ||
+      (humanChoice == "rock" && computerChoice == "scissor")
+    ) {
+      if (choice.indexOf(humanChoice) < choice.indexOf(computerChoice)) {
+        console.log(`you won ${humanChoice} beats ${computerChoice}`);
+        humanScore += 1;
+      } else {
+        console.log(`you lost ${computerChoice} beats ${humanChoice}`);
+        computerScore += 1;
+      }
+    } else {
+      if (choice.indexOf(humanChoice) > choice.indexOf(computerChoice)) {
+        console.log(`you won ${humanChoice} beats ${computerChoice}`);
+        humanScore += 1;
+      } else {
+        console.log(`you lost ${computerChoice} beats ${humanChoice}`);
+        computerScore += 1;
+      }
+    }
+    console.log(`Current score: human = ${humanScore}, computer = ${computerScore}`);   
+  }
+
+  for (let round = 0; round < 5; round++) {
+    let humanChoice = getHumanChoice();
+    let computerChoice = getComputerChoice();
+
+    playRound(humanChoice, computerChoice);
+    console.log("---------------------------------");
+  }
+
+  if (humanScore === computerScore) {
+    console.log("The end: Draw");
+  } else if (humanScore > computerScore) {
+    console.log(
+      `The End: You won!!, your Score ${humanScore} computer Score: ${computerScore}`
+    );
+  } else {
+    console.log(
+      `The End: you lost!!, your Score ${humanScore} computer Score: ${computerScore}`
+    );
+  }
+}
+
+playGame();
