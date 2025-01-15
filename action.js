397ffe0a3e17b4ee004479307d845ef7233d4afd
Abhay <actuallyabhay@gmail.com>
2025-01-15 09:09:29 +0000

    Player and computer choices done

--- action.js
+++ action.js
@@ -1,1 +1,14 @@
-console.log("Hello, world");
+const choice = ["Rock", "Paper", "Scissor"];
+
+function getComputerChoice() {
+  let randomIndex = Math.floor(Math.random() * choice.length);
+  console.log(`computer random choice is: ${choice[randomIndex]}\n`);
+}
+
+function getHumanChoice() {
+  let humanChoice = prompt("Rock/Paper/Scissors: ");
+  console.log(`Human choice is: ${humanChoice}`);
+}
+
+getComputerChoice();
+getHumanChoice();
